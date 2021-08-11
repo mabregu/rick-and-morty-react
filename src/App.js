f4a00f87838b22bca2e20984597197e87dfddc21
N. Martin Abregu <nmabregu@gmail.com>
2021-08-11 19:48:50 -0300

    config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+    return (
+        <div className='app'>
+            <h1>React project</h1>
+            <p>My first project with react js ;)</p>
+        </div>
+    )
+}
+
+export default App
